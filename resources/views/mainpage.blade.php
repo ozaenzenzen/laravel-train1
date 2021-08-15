7001c6ae8ac111ad96a41afd9bd95234d716b2a6
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-16 00:14:56 +0700

    add main page blade

---
+++ resources/views/mainpage.blade.php
@@ -0,0 +1,17 @@
+@extends('template')
+
+@section('content')
+<div class="p-3 mb-2 bg-success text-white">
+    <h1>Main Page Test</h1>
+</div>
+
+<div class="ml-4 container">
+    <div class="list-group">
+        <a href="/dosen" target="_blank" class="list-group-item list-group-item-action">Link to Dosen Page</a>
+        <a href="#" class="list-group-item list-group-item-action">Link to Page 1</a>
+        <a href="#" class="list-group-item list-group-item-action">Link to Page 2</a>
+        <a href="#" class="list-group-item list-group-item-action">Link to Page 3</a>
+
+    </div>
+</div>
+@stop
