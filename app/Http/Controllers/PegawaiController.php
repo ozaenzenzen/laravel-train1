355a6405ca80591ec45b757d92f37e57c91bc587
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-17 17:25:07 +0700

    9: crud (read) - add index method

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PegawaiController extends Controller
 {
-    public function index($nama)
+    public function index()
     {
-        return $nama;
+        // mengambil data dari table pegawai
+        $pegawai = DB::table('pegawai')->get();
+
+        // mengirim data pegawai ke view index
+        return view('/pegawai/index', ['pegawai'=>$pegawai]);
     }
 
     public function formulir()
@@ -16,9 +21,10 @@ class PegawaiController extends Controller
         return view('formulir');
     }
 
-    public function proses(Request $request){
+    public function proses(Request $request)
+    {
         $nama = $request->input('nama');
         $alamat = $request->input('alamat');
-        return "Nama : " .$nama. ", Alamat : " .$alamat;
+        return "Nama : " . $nama . ", Alamat : " . $alamat;
     }
 }
