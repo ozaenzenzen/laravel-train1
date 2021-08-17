358e22da64f9b32edb3906f3677c49d5e0a0f366
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-17 16:07:09 +0700

    8: template blade - add page blade

---
+++ resources/views/blog/home.blade.php
@@ -0,0 +1,42 @@
+@extends('template')
+
+@section('content')
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/blog">Navbar Blog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/blog">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/blog/tentang">Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/blog/kontak">Kontak</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown link
+                    </a>
+                    <ul class="dropdown-menu disabled" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div>
+    <h3>Halaman Blog</h3>
+    <h1 class="display-4">Halaman Blog</h1>
+</div>
+
+@stop
