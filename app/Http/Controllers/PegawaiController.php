6d66a1b214529463c9cc0368dd6e95c40a8a4fed
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-17 11:44:17 +0700

    5: request data - update controller

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -4,6 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PegawaiController extends Controller{
-    //
+class PegawaiController extends Controller
+{
+    public function index($nama)
+    {
+        return $nama;
+    }
+
+    public function formulir()
+    {
+        return view('formulir');
+    }
+
+    public function proses(Request $request){
+        $nama = $request->input('nama');
+        $alamat = $request->input('alamat');
+        return "Nama : " .$nama. ", Alamat : " .$alamat;
+    }
 }
