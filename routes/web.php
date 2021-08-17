3895e88e5705cc4fee549dd9d6193e1ce90ac894
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-17 16:05:50 +0700

    8: template blade - update route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
 use Illuminate\Support\Facades\Route;
@@ -31,3 +32,7 @@ Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
