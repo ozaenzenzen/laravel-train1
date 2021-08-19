fd88ab30abec1ef5ea7075236f0d9cbf987fcf43
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-19 07:13:53 +0700

    21: crud eloquent (create) - update index blade

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -1,36 +1,48 @@
 @extends('template')
 
 @section('content')
-<div class="p-2 container-fluid text-white" style="background-color: #f59542;">
+<div class="p-2 container-fluid text-white" style="background-color: #eb3486;">
     <h1 class="h1">Karyawan Page</h1>
 </div>
 
 <div class="container">
-    <div class="card-mt-4">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Alamat</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($karyawan as $k)
-                <tr>
-                    <td>{{ $k->id }}</td>
-                    <td>{{ $k->nama }}</td>
-                    <td>{{ $k->alamat }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        Halaman : {{ $karyawan->currentPage() }} <br />
-        Jumlah Data : {{ $karyawan->total() }} <br />
-        Data Per Halaman : {{ $karyawan->perPage() }} <br />
+    <div class="card my-3">
+        <div class="card-header text-center">
+            CRUD Data Karyawan - Eloquent
+        </div>
+        <div class="card-body">
+            <a href="/karyawan/tambah" class="btn btn-primary">Input Karyawan Baru</a>
+            <br><br>
+            <table class="table table-bordered table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Alamat</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($karyawan as $k)
+                    <tr>
+                        <td>{{ $k->id }}</td>
+                        <td>{{ $k->nama }}</td>
+                        <td>{{ $k->alamat }}</td>
+                        <td>
+                            <a href="/karyawan/edit/{{ $k->id }}" class="btn btn-warning">Edit</a>
+                            <a href="/karyawan/hapus/{{ $k->id }}" class="btn btn-danger">Hapus</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            Halaman : {{ $karyawan->currentPage() }} <br />
+            Jumlah Data : {{ $karyawan->total() }} <br />
+            Data Per Halaman : {{ $karyawan->perPage() }} <br />
 
 
-        {{ $karyawan->links('pagination::bootstrap-4') }}
+            {{ $karyawan->links('pagination::bootstrap-4') }}
+        </div>
     </div>
 </div>
 
