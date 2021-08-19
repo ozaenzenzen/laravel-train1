c9d28b3f62244a0e7a4713c4539834fb6992dca8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-19 07:13:31 +0700

    21: crud eloquent (create) - add store method

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -16,4 +16,30 @@ class KaryawanController extends Controller
 
         return view('/karyawan/index', ['karyawan' => $karyawan]);
     }
+
+    public function tambah()
+    {
+        return view('/karyawan/tambah');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama' => 'required',
+            'alamat' => 'required',
+        ]);
+
+        Karyawan::create([
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+        ]);
+
+        return redirect('/karyawan');
+    }
+
+    public function edit($id)
+    {
+        $karyawan = Karyawan::find($id);
+        return view('/karyawan/edit', ['karyawan' => $karyawan]);
+    }
 }
