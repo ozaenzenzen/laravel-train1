40c7d60685dda8a75b2f50820eccf22fb5e44315
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-25 00:19:27 +0700

    23: one to one - add telepon models

---
+++ app/Models/Telepon.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Telepon extends Model
+{
+    use HasFactory;
+
+    protected $table = "telepon";
+
+    public function pengguna()
+    {
+        return $this->belongsTo('App\Models\Pengguna');
+        // return $this->belongsTo('app\Models\Pengguna');
+    }
+}
