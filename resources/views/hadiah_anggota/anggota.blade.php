8c5b49c157b4f5867c8385998807c64ec21c2b7c
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-08-26 21:46:42 +0700

    25: many to many - add anggota blade

---
+++ resources/views/hadiah_anggota/anggota.blade.php
@@ -0,0 +1,40 @@
+@extends('template')
+
+@section('content')
+<div class="p-2 container-fluid" style="background-color: greenyellow; color:black">
+    <h1 class="h1">Anggota Page: Many to many</h1>
+</div>
+
+<div class="container">
+    <div class="card mt-5">
+        <div class="card-body">
+            <h5 class="text-center my-4">Eloquent Many to Many</h5>
+            <table class="table table-bordered table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th>Nama Pengguna</th>
+                        <th>Hadiah</th>
+                        <th width="18%">Jumlah</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($anggota as $a)
+                    <tr>
+                        <td>{{ $a->nama }}</td>
+                        <td>
+                            <ul>
+                                @foreach($a->hadiah as $h)
+                                <li>{{ $h->nama_hadiah }}</li>
+                                @endforeach
+                            </ul>
+                        </td>
+                        <td class="text-center">{{ $a->hadiah->count() }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+@stop
